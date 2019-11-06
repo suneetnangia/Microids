a566bbe4f0bea7d81ef875227b047a2374585db9
ScottGrosenick <scogro@microsoft.com>
2019-11-05 16:10:16 -0800

    Initial commit for wiring up App Insights

--- Microsoft.OneWeek.Hack.Microids.MessageRouter/Program.cs
+++ Microsoft.OneWeek.Hack.Microids.MessageRouter/Program.cs
@@ -5,6 +5,11 @@
     using System.Threading;
     using System.Runtime.Loader;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights.DependencyCollector;
+    using Microsoft.Extensions.Configuration;
+    using System.Net.Http;
 
     public class Program
     {
@@ -18,8 +23,16 @@
                 AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
                 Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
 
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Environment.CurrentDirectory)
+                    .AddJsonFile("local.settings.json", true)
+                    .Build();
+
                 // Bootstrap services using dependency injection.
-                var services = new ServiceCollection();
+                var services = new ServiceCollection()
+                    .AddSingleton<IConfiguration>(configuration)
+                    .AddSingleton<TelemetryClient>(ConstructTelemetryClient(configuration));
+
                 // TODO: confiure all services here e.g. IDataSink, IDataSource, IDeviceDataEnricher etc. 
 
                 // Dispose method of ServiceProvider will dispose all disposable objects constructed by it as well.
@@ -28,10 +41,43 @@
                     // Get a new message router object.
                     var messagerouter = serviceProvider.GetService<EnrichmentMessageRouter>();
                     messagerouter.Initiate(cts.Token);
-
                     await WhenCancelled(cts.Token);        
                 }
             }
+        }
+
+        private static TelemetryClient ConstructTelemetryClient(IConfiguration config)
+        {
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+            configuration.InstrumentationKey = config.GetValue<string>("AppInsightsKey");
+            configuration.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
+            InitializeDependencyTracking(configuration);
+
+            return new TelemetryClient(configuration);
+        }
+
+        private static DependencyTrackingTelemetryModule InitializeDependencyTracking(TelemetryConfiguration configuration)
+        {
+            var module = new DependencyTrackingTelemetryModule();
+
+            // prevent Correlation Id to be sent to certain endpoints. You may add other domains as needed.
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.windows.net");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.chinacloudapi.cn");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.cloudapi.de");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.usgovcloudapi.net");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("localhost");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("127.0.0.1");
+
+            // enable known dependency tracking, note that in future versions, we will extend this list. 
+            // please check default settings in https://github.com/Microsoft/ApplicationInsights-dotnet-server/blob/develop/Src/DependencyCollector/DependencyCollector/ApplicationInsights.config.install.xdt
+
+            module.IncludeDiagnosticSourceActivities.Add("Microsoft.Azure.ServiceBus");
+            module.IncludeDiagnosticSourceActivities.Add("Microsoft.Azure.EventHubs");
+
+            // initialize the module
+            module.Initialize(configuration);
+
+            return module;
         }
 
         public static Task WhenCancelled(CancellationToken cancellationToken)
